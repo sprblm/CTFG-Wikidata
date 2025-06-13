df5a6df648413469de1ba4aef878311e629dec21
Tim Eccleston <timothy.perisho.eccleston@gmail.com>
2025-06-13 09:54:06 -0700

    turn airtable updating matching IDs back on

--- sync.py
+++ sync.py
@@ -113,7 +113,7 @@ for bucket, count in sorted(count_of_counts.items()):
 
 
 update_matches_message = 'Updating CTFG with matching wikibase IDs...'
-if False:
+if True:
   log(update_matches_message)
   ctfg.batch_update([{'id': key, 'fields': {'Wikidata ID suggestions': '\n'.join(matches)}} for (key, matches) in wiki_matches.items()])
 else:
