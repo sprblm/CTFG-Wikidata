e96f3b3f379cdb463366e62a222edc96dbe4d200
Tim Eccleston <timothy.perisho.eccleston@gmail.com>
2025-08-13 14:14:30 -0700

    :warning: UNTESTED: move wikibase user agent into config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Optional
 from dotenv import load_dotenv
+from wikibaseintegrator.wbi_config import config as wbi_config
+from wikibaseintegrator import WikibaseIntegrator
 
 load_dotenv()
 
@@ -15,3 +17,10 @@ READ_CTFG_FROM_CACHE: bool = "TRUE".startswith(
 class airtable:
     api_key: str = os.environ["AIRTABLE_API_KEY"]
     base_id: str = os.environ["AIRTABLE_BASE_ID"]
+
+
+wbi_config["USER_AGENT"] = (
+    "AutomationDev/0.1 (https://www.wikidata.org/wiki/User:TECCLESTON-TECH"
+)
+
+wbi = WikibaseIntegrator()
