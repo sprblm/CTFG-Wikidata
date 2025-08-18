051544361d73394c20acead032f2e46f7272ab73
Tim Eccleston <timothy.perisho.eccleston@gmail.com>
2025-08-17 22:53:07 -0700

    print lens of wiki creds to verify

--- src/config.py
+++ src/config.py
@@ -29,6 +29,9 @@ wbi_config["USER_AGENT"] = (
 )
 
 if WIKIDATA_BOT_USERNAME and WIKIDATA_BOT_PW:
+    print(len(WIKIDATA_BOT_USERNAME))
+    print(len(WIKIDATA_BOT_PW))
+
     wbi_login.Login(user=WIKIDATA_BOT_USERNAME, password=WIKIDATA_BOT_PW)
 
 wbi = WikibaseIntegrator()
